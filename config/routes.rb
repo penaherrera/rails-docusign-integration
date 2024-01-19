349b1599104fc69e4af592cba330c2d3974edd9b
penaherrera <cpena@ioogo.com>
2024-01-19 13:19:42 -0600

    Adding Scaffold for Users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :users do
+    get 'request_signature', on: :member
+  end
+
+  root to: 'users#new'
+  # get 'users/request_signature', to: 'users#request_signature'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
